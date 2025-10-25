fa6178b1554db7823a0245aa51a4be9700866d10
Retreller <valeians.2005@gmail.com>
2025-10-25 17:08:06 +0000

    Homework 3

--- hello_world/hello_world.cpp
+++ hello_world/hello_world.cpp
@@ -1,10 +1,7 @@
-//
-// Created by valeri on 10/25/25.
-//
-
 #include <iostream>
 
 int main(int, char**) {
-std::cout << "Hello World\n";
-return 0;
+    std::cout << "Hello, World!" << std::endl;
+
+    return 0;
 }
