eca7cf305a7e8b93d5667fc18497852f1805f22a
Valeri Aleksanyan <114948873+Retreller@users.noreply.github.com>
2026-03-31 23:44:51 +0400

    Refactor main to use lambda for producer and consumer
    Refactor producer and consumer to use lambda functions and update blocking queue usage.

--- hw13_bl_queue/demo/main.cpp
+++ hw13_bl_queue/demo/main.cpp
@@ -1,32 +1,39 @@
+#include "ArmenianQueue.h"
 #include <iostream>
 #include <thread>
-#include <chrono>
-#include "ArmenianQueue.h"
-
-blocking_queue<int> bq(5);
-
-void producer() {
-    for (int i = 1; i <= 10; ++i) {
-        bq.push(i);
-        std::cout << "Produced: " << i << std::endl;
-        std::this_thread::sleep_for(std::chrono::milliseconds(200));
-    }
-}
-
-void consumer() {
-    for (int i = 1; i <= 10; ++i) {
-        int x = bq.pop();
-        std::cout << "Consumed: " << x << std::endl;
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
-    }
-}
+#include <vector>
 
 int main() {
-    std::thread t1(producer);
-    std::thread t2(consumer);
+    const int iterations = 6;
+    rau::blocking_queue<int> q(iterations);
 
-    if (t1.joinable()) t1.join();
-    if (t2.joinable()) t2.join();
+    std::thread producer([&] {
+        for (int i = 1; i <= iterations; ++i) {
+            q.push(i);
+            std::cout << "Produced: " << i << std::endl;
+        }
+    });
+
+    std::thread consumer([&] {
+        for (int i = 0; i < iterations; ++i) {
+            const auto val = q.pop();
+            std::cout << "Consumed: " << val << std::endl;
+        }
+    });
+
+    producer.join();
+    consumer.join();
+
+    rau::blocking_queue<int> q2(5);
+    q2.push(100);
+
+    if (auto val = q2.try_pop()) {
+        std::cout << "try_pop result: " << *val << std::endl;
+    }
+
+    if (auto val = q2.try_pop(); !val) {
+        std::cout << "try_pop result: empty" << std::endl;
+    }
 
     return 0;
 }
