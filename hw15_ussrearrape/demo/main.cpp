0d36852c5f11da1deb6d3bc3469dc5c7ca44cb83
Valeri Aleksanyan <114948873+Retreller@users.noreply.github.com>
2026-04-03 13:43:23 +0400

    Update main.cpp

--- hw15_ussrearrape/demo/main.cpp
+++ hw15_ussrearrape/demo/main.cpp
@@ -16,10 +16,6 @@ int main() {
     std::vector<int> pid_list = {1234, 5678, 9101, 1121, 3141};
     pid_list.push_back(spy_pid);
 
-    std::random_device rd;
-    std::mt19937 g(rd());
-    std::shuffle(pid_list.begin(), pid_list.end(), g);
-
     std::ofstream ofs("/tmp/reichspost.txt");
     for (int p : pid_list) ofs << p << " ";
     ofs.close();
